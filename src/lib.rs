74c872168e60ca2107f54cd1f05a853e65dfee25
Niko Lepka <slench102+git@gmail.com>
2018-03-27 21:12:15 +0200

    add toml and source

---
+++ src/lib.rs
@@ -0,0 +1,124 @@
+mod futaba_vfd {
+    extern crate sysfs_gpio;
+    use sysfs_gpio as gpio;
+    use std::thread::sleep;
+    use std::time::Duration;
+
+    pub const CLEAR_DISPLAY: u8 = 0x01;
+    pub const SET_CURSOR: u8 = 0x02;
+    pub const SET_INTENSITY: u8 = 0x04;
+
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    pub struct FutabaVFD {
+        clock_pin: gpio::Pin,
+        data_pin: gpio::Pin,
+        strobe_pin: gpio::Pin,
+        column_count: u8,
+        intensity: u8,
+    }
+
+    impl FutabaVFD {
+        pub fn new(clock: u64, data: u64, strobe: u64, column_count: u8, intensity: u8) -> FutabaVFD {
+            let clock_pin = gpio::Pin::new(clock);
+            let data_pin = gpio::Pin::new(data);
+            let strobe_pin = gpio::Pin::new(strobe)
+            FutabaVFD { clock_pin, data_pin, strobe_pin, column_count, intensity }
+        }
+
+        // sets up all the pins and marks them as outputs
+        pub fn begin(&self) -> gpio::Result<()> {
+            self.clock_pin.export()?;
+            self.data_pin.export()?;
+            self.strobe_pin.export()?;
+
+            self.clock_pin.set_direction(gpio::Direction::Out)?;
+            self.data_pin.set_direction(gpio::Direction::Out)?;
+            self.strobe_pin.set_direction(gpio::Direction::Out)?;
+
+            delay(500);
+            self.strobe()?;
+            self.send(SET_INTENSITY)?;
+            self.send(self.intensity)?;
+            self.clear()?;
+            Ok(())
+        }
+
+        // unexports all the pins
+        pub fn end(&self) -> gpio::Result<()> {
+            self.clock_pin.unexport()?;
+            self.data_pin.unexport()?;
+            self.strobe_pin.unexport()?;
+            Ok(())
+        }
+
+        // Lets the user run the display within a closure, rather than having to
+        // remember to begin and end the display.
+        pub fn with_beginning<F: FnOnce() -> Result<()>>(&self, closure: F) -> Result<()> {
+            self.begin()?;
+            match closure() {
+                Ok(()) => {
+                    try!(self.end());
+                    Ok(())
+                },
+                Err(e) => {
+                    self.end()?;
+                    Err(e)
+                }
+            }
+        }
+
+        pub fn clear(&self) -> gpio::Result<()> {
+            self.send(CLEAR_DISPLAY)
+        }
+
+        pub fn home(&self) -> gpio::Result<()> {
+            self.set_cursor(0, 0)
+        }
+
+        pub fn set_cursor(&self, col: u8, row: u8) -> gpio::Result<()> {
+            self.send(SET_CURSOR)?;
+            self.send(row * self.column_count + col + 1)
+        }
+
+        pub fn send(&self, data: u8) -> gpio::Result<()> {
+            let mut mask: u8 = 0x80;
+            for i in 0 .. 8 {
+                self.clock_pin.set_value(1)?;
+                delay_micros(15);
+                self.data_pin.set_value(!!(data & mask))?;
+                delay_micros(5);
+                self.clock_pin::set_value(0)?;
+
+                mask = mask >> 1;
+            }
+            Ok(())
+        }
+
+        pub fn strobe(&self) -> gpio::Result<()> {
+            self.clock_pin.set_value(1)?;
+            self.data_pin.set_value(1)?;
+
+            self.strobe_pin.set_value(1)?;
+            delay(5);
+            self.strobe_pin.set_value(0)?;
+            delay(5);
+            Ok(())
+        }
+    }
+
+    // convenience function
+    fn delay(time: u64) {
+        sleep(Duration::from_millis(time));
+    }
+
+    fn delay_micros(time: u64) {
+        sleep(Duration::from_micros(time));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
